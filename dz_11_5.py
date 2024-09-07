722976932d706dfdc887758678987e6ecf489998
Andrei Burvel <burvelandrei@gmail.com>
2024-09-07 22:16:45 +0300

    добавлена задача №5

---
+++ dz_11_5.py
@@ -0,0 +1,26 @@
+class SuperStr(str):
+    """
+    Класс  расширяющий возможности стандартного класса str,
+    добавлена проверка на вхождение переданной строки в текущу
+    и проверка на палиндром
+    """
+
+    def __init__(self, string: str):
+        self.string = string
+
+    def is_repeatance(self, sample: str) -> bool:
+        count_repeat = len(self.string) // (len(sample) or 1)
+        return self.string == sample * count_repeat
+
+    def is_palindrom(self) -> bool:
+        return self.string.lower() == self.string.lower()[::-1]
+
+
+print("-" * 10)
+super_str = SuperStr("abc")
+print(super_str.is_palindrom())
+print("-" * 10)
+super_str_2 = SuperStr("abcdeabcde")
+print(super_str_2.is_repeatance("abc"))
+print(super_str_2.is_repeatance("abcde"))
+print("-" * 10)
