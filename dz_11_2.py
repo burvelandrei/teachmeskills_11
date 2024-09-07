8cc853121866fe2b0d32e5c36aed57e4989a6eaa
Andrei Burvel <burvelandrei@gmail.com>
2024-09-07 14:12:34 +0300

    добавлена задача №2

---
+++ dz_11_2.py
@@ -0,0 +1,37 @@
+class Math:
+    """Класс для реализации некоторых функций калькулятора"""
+
+    def addition(self, number_1: int | float, number_2: int | float) -> int | float:
+        try:
+            print(number_1 + number_2)
+        except TypeError:
+            print("Нужно передать числа!")
+
+    def subtraction(self, number_1: int | float, number_2: int | float) -> int | float:
+        try:
+            print(number_1 - number_2)
+        except TypeError:
+            print("Нужно передать числа!")
+
+    def multiplication(
+        self, number_1: int | float, number_2: int | float
+    ) -> int | float:
+        try:
+            print(number_1 * number_2)
+        except TypeError:
+            print("Нужно передать числа!")
+
+    def division(self, number_1: int | float, number_2: int | float) -> int | float:
+        try:
+            print(number_1 / number_2)
+        except ZeroDivisionError:
+            print("На ноль делить нельзя!")
+        except TypeError:
+            print("Нужно передать числа!")
+
+
+Calc = Math()
+Calc.addition(3, 4)
+Calc.subtraction(5, 4)
+Calc.multiplication(5, 5)
+Calc.division(5, 5)
