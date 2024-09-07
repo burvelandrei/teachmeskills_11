76e8c36bae1c34d2e5ac0c985bc344dc68566df5
Andrei Burvel <burvelandrei@gmail.com>
2024-09-07 22:33:04 +0300

    добавил проверку на пустую строку и подкооректировал документацию

--- dz_11_5.py
+++ dz_11_5.py
@@ -1,7 +1,7 @@
 class SuperStr(str):
     """
     Класс  расширяющий возможности стандартного класса str,
-    добавлена проверка на вхождение переданной строки в текущу
+    добавлена проверка на вхождение переданной строки в текущую
     и проверка на палиндром
     """
 
@@ -9,6 +9,8 @@ class SuperStr(str):
         self.string = string
 
     def is_repeatance(self, sample: str) -> bool:
+        if not len(self.string):
+            return False
         count_repeat = len(self.string) // (len(sample) or 1)
         return self.string == sample * count_repeat
 
@@ -23,4 +25,7 @@ print("-" * 10)
 super_str_2 = SuperStr("abcdeabcde")
 print(super_str_2.is_repeatance("abc"))
 print(super_str_2.is_repeatance("abcde"))
+print("-" * 10)
+super_str_3 = SuperStr("")
+print(super_str_3.is_repeatance(""))
 print("-" * 10)
